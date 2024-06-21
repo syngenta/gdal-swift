9b6431b611af36b60b4426d1d384d0574c03eeef
Yevhenii Kalashnikov <lumyk@me.com>
2024-06-21 14:05:41 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,10 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "gdal",
+            // This URL should be updated after GDAL recompile
             url: "https://github.com/syngenta/gdal-swift/releases/download/1.0.0/gdal.xcframework.zip",
+            // After changing the URL, you should also update the 'checksum'.
+            // Run 'swift package compute-checksum gdal.xcframework.zip' command to get the 'checksum'.
             checksum: "d786dcb6b38927859d98008a8ad51be5aace98d77335befee88bfd7697ca3b81"
         ),
         .target(
